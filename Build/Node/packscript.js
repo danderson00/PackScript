dfb3352b8778ec12bbb3f9d449355982b2a4a957
danderson00 <danderson00@gmail.com>
2013-12-28 16:46:57 +1100

    Fixed issue with syncTo and directory

--- Build/Node/packscript.js
+++ Build/Node/packscript.js
@@ -836,18 +836,20 @@ Pack.transforms.outputTemplate = {
 Pack.transforms.syncTo = {
     event: 'finalise',
     apply: function(data, pack) {
-        var Files = Pack.api.Files;
-        var Log = Pack.api.Log;
+        var Files = Pack.api.Files,
+            Log = Pack.api.Log,
+            output = data.output;
 
-        var targetFolder = Path(data.output.basePath + data.value + '/');
-        var sourceDirectory = data.output.transforms.directory;
+        var targetFolder = Path(output.basePath + data.value + '/');
+        var sourceDirectory = output.transforms.directory;
         
         if (sourceDirectory) {
             // sync an entire folder
+            sourceDirectory = Path(output.basePath + sourceDirectory + '/').toString();
             Files.copy(sourceDirectory, targetFolder.toString());
             Log.info('Copied directory ' + sourceDirectory + ' to ' + targetFolder);
 
-            data.output.getCurrentPaths = function() {
+            output.getCurrentPaths = function() {
                 // match the folder sync when any of the files change
                 return Files.getFilenames(sourceDirectory + '/*.*', true);
             };
@@ -860,7 +862,7 @@ Pack.transforms.syncTo = {
             Log.info('Copied ' + files.length + ' files to ' + targetFolder);
 
             // this is a bit nasty. It is consumed by Output.matches
-            data.output.currentPaths = data.target.files && data.target.files.paths();
+            output.currentPaths = data.target.files && data.target.files.paths();
         }
     }
 };
--- Core/Transforms/syncTo.js
+++ Core/Transforms/syncTo.js
@@ -1,18 +1,20 @@
 ﻿Pack.transforms.syncTo = {
     event: 'finalise',
     apply: function(data, pack) {
-        var Files = Pack.api.Files;
-        var Log = Pack.api.Log;
+        var Files = Pack.api.Files,
+            Log = Pack.api.Log,
+            output = data.output;
 
-        var targetFolder = Path(data.output.basePath + data.value + '/');
-        var sourceDirectory = data.output.transforms.directory;
+        var targetFolder = Path(output.basePath + data.value + '/');
+        var sourceDirectory = output.transforms.directory;
         
         if (sourceDirectory) {
             // sync an entire folder
+            sourceDirectory = Path(output.basePath + sourceDirectory + '/').toString();
             Files.copy(sourceDirectory, targetFolder.toString());
             Log.info('Copied directory ' + sourceDirectory + ' to ' + targetFolder);
 
-            data.output.getCurrentPaths = function() {
+            output.getCurrentPaths = function() {
                 // match the folder sync when any of the files change
                 return Files.getFilenames(sourceDirectory + '/*.*', true);
             };
@@ -25,7 +27,7 @@
             Log.info('Copied ' + files.length + ' files to ' + targetFolder);
 
             // this is a bit nasty. It is consumed by Output.matches
-            data.output.currentPaths = data.target.files && data.target.files.paths();
+            output.currentPaths = data.target.files && data.target.files.paths();
         }
     }
 };
